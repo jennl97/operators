45a50fb4c18baf571b96e1bf97314bc559f82bc0
jennl97 <jennhott97@gmail.com>
2016-09-19 15:29:10 -0500

    inital upload of critical thinking exercise 2

---
+++ Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//based on someone who is 5ft 6 in, average steps to a mile is 2324
+
+let stepsInMiles: Int = 2324
+var milesWalked: Int
+milesWalked = 5
+let totalSteps = stepsInMiles * milesWalked
+let description = "You have walked \(totalSteps) today"
+
+
+print(stepsInMiles * milesWalked)
+
+print(description)
+
+
+
+
+
